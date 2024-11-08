068a7bdffbc95224dd10a84e96646a390d0bf83f
gh0ststudent <buhoge.k@gmail.com>
2024-11-08 17:54:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ballet',
+    'O que é Demi-Plié?',
+    'É a meia flexão dos joelhos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é 826 : 4',
+    'O resultado é 206,5'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Fast em Inglês?',
+    'Fast em ingles é Rapido '
+)
